65852576d19059043f4628562e265967b8be7371
Nyssim <viniciusjunioribeiro05@gmail.com>
2024-11-08 14:48:15 -0300

    implementação do layout da pagina de orientações

--- ARTSABOARIA/src/components/Orientation/OrientationPage.vue
+++ ARTSABOARIA/src/components/Orientation/OrientationPage.vue
@@ -1,21 +1,33 @@
+<script setup lang="ts">
+
+    import './OrientationPage.css'
+
+</script>
+
 <template>
-    <main>
-        <article>
-            <div class="titles">
-                <h1>Como posso fazer o descarte consiente dos meus sabonetes.</h1>
-            </div>
-            <p>Uma iniciativa da <span>ARTSABOARIA LTDA</span>, com o proposito de diminuir o descarte incorreto dos sabonetes que provoca danos para o meio ambiente</p>
-            <h1>Formas devidas</h1>
-            <ol start="1">
-                <li>Usar o sabonete até o fim</li>
-                <li>Reciclar o sabonete</li>
-                <li>Compostagem</li>
-                <li>Reutilização criativa</li>
-                <li>Evite embalagens desnecessárias</li>
-            </ol>
-        </article>
-        <aside>
-            <img src="../../assets/images/Imagem ilustrativa.svg" alt="">
-        </aside>
-    </main>
+    <div id="container_OrientationPage">
+        <main>
+            <article>
+                <div class="titles">
+                    <h1>Como posso fazer o descarte consiente dos meus <span>sabonetes</span>.</h1>
+                </div>
+                <div id="description">
+                    <p>Uma iniciativa da <span>ARTSABOARIA LTDA</span>, com o proposito de diminuir o descarte incorreto dos sabonetes que provoca danos para o meio ambiente</p>
+                </div>
+                <div class="titles">
+                    <h1>Formas devidas</h1>
+                </div>
+                <ol start="1">
+                    <li>Usar o sabonete até o fim</li>
+                    <li>Reciclar o sabonete</li>
+                    <li>Compostagem</li>
+                    <li>Reutilização criativa</li>
+                    <li>Evite embalagens desnecessárias</li>
+                </ol>
+            </article>
+            <aside>
+                <img src="../../assets/images/Imagem ilustrativa.svg" alt="">
+            </aside>
+        </main>
+    </div>
 </template>
--- ARTSABOARIA/src/components/main_inicio/MainLaddingPage.vue
+++ ARTSABOARIA/src/components/main_inicio/MainLaddingPage.vue
@@ -4,21 +4,23 @@
 </script>
 
 <template>
-    <main>
-        <section>
-            <div id="text">
-                <h1>Quem nós <span>somos</span>?</h1><br>
-                <h3>Nós somos uma pequena empresa familiar, fundada recentemente em <span>Búzios-RJ</span> com o intuito de gerar não somente renda mas também movimentar a economia local, gerar empregos e encorajar as pessoas a serem quem elas realmente são, aceitando a si mesmas como elas realmente são, e incentivando-as a cuidarem de si <span>mesmas</span>.</h3>
-            </div>
-            <div id="interaction">
-                <RouterLink to="/galeria">
-                    <button>GALERIA</button>
-                </RouterLink>
-            </div>
-        </section>
-        <aside>
-            <img id="card-image" src="https://images.pexels.com/photos/7500307/pexels-photo-7500307.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-        </aside>
-    </main>
+    <div id="container_LaddingPage">
+        <main>
+            <section>
+                <div id="text">
+                    <h1>Quem nós <span>somos</span>?</h1><br>
+                    <h3>Nós somos uma pequena empresa familiar, fundada recentemente em <span>Búzios-RJ</span> com o intuito de gerar não somente renda mas também movimentar a economia local, gerar empregos e encorajar as pessoas a serem quem elas realmente são, aceitando a si mesmas como elas realmente são, e incentivando-as a cuidarem de si <span>mesmas</span>.</h3>
+                </div>
+                <div id="interaction">
+                    <RouterLink to="/galeria">
+                        <button>GALERIA</button>
+                    </RouterLink>
+                </div>
+            </section>
+            <aside>
+                <img id="card-image" src="https://images.pexels.com/photos/7500307/pexels-photo-7500307.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+            </aside>
+        </main>
+    </div>
     <RouterView />
 </template>
