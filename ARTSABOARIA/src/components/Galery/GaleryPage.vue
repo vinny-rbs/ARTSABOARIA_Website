931652e09dd44e1e3c22b2a68fa15c550f10e25c
Nyssim <viniciusjunioribeiro05@gmail.com>
2024-11-14 16:28:50 -0300

    organização dos elementos da galeria

--- ARTSABOARIA/src/components/Galery/GaleryPage.vue
+++ ARTSABOARIA/src/components/Galery/GaleryPage.vue
@@ -1,20 +1,105 @@
 <script>
-
-    import './GaleryPage.css'
+    import './GaleryPage.css';
 
 </script>
 
 <template>
 
     <div id="container_GaleryPage">
-        <div class="title">
-            <h1>GALERIA</h1>
+        <div id="info_top_GaleryPage">
+            <div class="title">
+                <h1>GALERIA</h1>
+            </div>
+            <div id="search_field">
+                <input type="search">
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#f7f6f3" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                </button>
+            </div>
         </div>
-        <div id="search_field">
-            <input type="search">
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#f7f6f3" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-            </button>
+        <div id="grid_product">
+            <!--Cards-->
+            <div class="card_product">
+                <div id="info_top_card">
+                    <img src="https://images.pexels.com/photos/10853724/pexels-photo-10853724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div id="info_bottom_card">
+                    <div id="description">
+                        <h3>Sabonete de Rosas Vermelhas</h3>
+                        <p><span id="gray_caption">Sabonetes de frutas tropicais</span></p>
+                        <p>Quantidade em estoque: <span id="orange_caption">20</span></p>
+                    </div>
+                    <div id="interactivity">
+                        <button>Comprar</button>
+                        <h3>R$ <span id="orange_caption">25,90</span></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="card_product">
+                <div id="info_top_card">
+                    <img src="https://images.pexels.com/photos/10853724/pexels-photo-10853724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div id="info_bottom_card">
+                    <div id="description">
+                        <h3>Sabonete de Rosas Vermelhas</h3>
+                        <p><span id="gray_caption">Sabonetes de frutas tropicais</span></p>
+                        <p>Quantidade em estoque: <span id="orange_caption">20</span></p>
+                    </div>
+                    <div id="interactivity">
+                        <button>Comprar</button>
+                        <h3>R$ <span id="orange_caption">25,90</span></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="card_product">
+                <div id="info_top_card">
+                    <img src="https://images.pexels.com/photos/10853724/pexels-photo-10853724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div id="info_bottom_card">
+                    <div id="description">
+                        <h3>Sabonete de Rosas Vermelhas</h3>
+                        <p><span id="gray_caption">Sabonetes de frutas tropicais</span></p>
+                        <p>Quantidade em estoque: <span id="orange_caption">20</span></p>
+                    </div>
+                    <div id="interactivity">
+                        <button>Comprar</button>
+                        <h3>R$ <span id="orange_caption">25,90</span></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="card_product">
+                <div id="info_top_card">
+                    <img src="https://images.pexels.com/photos/10853724/pexels-photo-10853724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div id="info_bottom_card">
+                    <div id="description">
+                        <h3>Sabonete de Rosas Vermelhas</h3>
+                        <p><span id="gray_caption">Sabonetes de frutas tropicais</span></p>
+                        <p>Quantidade em estoque: <span id="orange_caption">20</span></p>
+                    </div>
+                    <div id="interactivity">
+                        <button>Comprar</button>
+                        <h3>R$ <span id="orange_caption">25,90</span></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="card_product">
+                <div id="info_top_card">
+                    <img src="https://images.pexels.com/photos/10853724/pexels-photo-10853724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div id="info_bottom_card">
+                    <div id="description">
+                        <h3>Sabonete de Rosas Vermelhas</h3>
+                        <p><span id="gray_caption">Sabonetes de frutas tropicais</span></p>
+                        <p>Quantidade em estoque: <span id="orange_caption">20</span></p>
+                    </div>
+                    <div id="interactivity">
+                        <button>Comprar</button>
+                        <h3>R$ <span id="orange_caption">25,90</span></h3>
+                    </div>
+                </div>
+            </div> 
+            <!--Final Card-->
         </div>
     </div>
 
