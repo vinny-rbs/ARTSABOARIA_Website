b41a2d4a3d41f3d630a53b9047bfdc7998da912f
Nyssim <viniciusjunioribeiro05@gmail.com>
2024-12-01 17:20:40 -0300

    teste

--- ARTSABOARIA/src/views/Galeria.vue
+++ ARTSABOARIA/src/views/Galeria.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-<<<<<<< HEAD
-
-import GaleryPage from '../components/Galery/GaleryPage.vue';
-=======
     import GaleryPage from '@/components/Galery/GaleryPage.vue';
->>>>>>> frontend
 </script>
 
 <template>
