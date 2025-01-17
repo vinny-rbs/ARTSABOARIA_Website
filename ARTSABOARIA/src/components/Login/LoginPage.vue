c363bbec16185c422e4353e92acca46ec890af2c
Vinicius Ribeiro da Silva <viniciusjunioribeiro05@gmail.com>
2025-01-17 20:51:52 -0300

    implementação do login

--- ARTSABOARIA/src/components/Login/LoginPage.vue
+++ ARTSABOARIA/src/components/Login/LoginPage.vue
@@ -1,3 +1,24 @@
+<script>
+
+import '../Login/styles/Login.css';
+
+</script>
+
 <template>
-    <h1>Login</h1>
+    <div class="container-LoginPage">
+        <div class="card-LoginPage">
+            <aside id="aside-LoginPage">
+                <h1>Bem-vindo de volta!</h1>
+                <p>De volta para o site</p>
+            </aside>
+            <article id="article-LoginPage">
+                    <h1>Faça o Login</h1>
+                <form action="#" id="form-LoginPage">
+                    <input type="email" placeholder="Email">
+                    <input type="password" placeholder="senha">
+                    <input type="submit">
+                </form>
+            </article>
+        </div>
+    </div>
 </template>
